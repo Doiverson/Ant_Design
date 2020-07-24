21cfe6a8dfe4539376c127c09b4bdcd492c613e7
ShosukeDoi <doiverson.canada@gmail.com>
2020-07-24 15:02:42 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { Button, DatePicker, message, Alert } from 'antd';
+
+import Table from './Table';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [date, setDate] = useState(null);
+    const handleChange = value => {
+        message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+        setDate(value);
+    };
+    return (
+        <div style={{ width: 400, margin: '100px auto' }}>
+            <DatePicker onChange={handleChange} />
+            <div style={{ marginTop: 16 }}>
+                <Alert message="Selected Date" description={date ? date.format('YYYY-MM-DD') : 'None'} />
+            </div>
+            <Child1/>
+            <div style={{marginTop: 20}}/>
+            <Table/>
+        </div>
+    );
 }
 
 export default App;
+
+
+class Child1 extends React.Component {
+
+    state = {
+        loadings: [],
+    };
+
+    enterLoading = index => {
+        this.setState(({ loadings }) => {
+            const newLoadings = [...loadings];
+            newLoadings[index] = true;
+
+            return {
+                loadings: newLoadings,
+            };
+        });
+        setTimeout(() => {
+            this.setState(({ loadings }) => {
+                const newLoadings = [...loadings];
+                newLoadings[index] = false;
+
+                return {
+                    loadings: newLoadings,
+                };
+            });
+        }, 6000);
+    };
+
+    render() {
+        const { loadings } = this.state;
+        return(
+            <Button className="test" type="primary" loading={loadings[0]} onClick={() => this.enterLoading(0)}>
+                Click me!
+            </Button>
+        )
+    }
+}
